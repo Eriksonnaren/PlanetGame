1699a75e6706566b20aef4d7c1e163cf27dd3cd4
Erik Hörnvall <erik.hornvall@taftea.se>
2017-04-22 11:03:21 +0200

    eriksonn

--- Planet Game 4/Planet Game 4/Form1.cs
+++ Planet Game 4/Planet Game 4/Form1.cs
@@ -16,5 +16,10 @@ namespace Planet_Game_4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Eriksonn");
+        }
     }
 }
