f5419190a3e08571eccbb111e8fdbfde8699ad09
John Hörnvall <john.minecraft@hotmail.se>
2017-04-22 11:26:49 +0200

    Added uis and the main ui for the game

---
+++ Planet Game 4/Planet Game 4/theGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planet_Game_4
+{
+    public class theGame : ui
+    {
+
+        Graphics graphics;
+
+        public theGame(Graphics graphics)
+        {
+            // You do need an object to display all the stuff to the screen, do you not?
+            this.graphics = graphics;
+        }
+
+        // Do physics and calculations
+        public void update()
+        {
+
+        }
+
+        // Do visual calculations and display all the things
+        public void show()
+        {
+
+        }
+
+    }
+}
---
+++ Planet Game 4/Planet Game 4/ui.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planet_Game_4
+{
+    public interface ui
+    {
+        // Updates the interface, like physics or something
+        void update();
+
+        // Displays the menu
+        void show();
+
+    }
+}
