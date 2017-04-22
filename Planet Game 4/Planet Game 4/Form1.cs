7e1a82eaa34a85c133faa866c77ac6c764554108
Erik Hörnvall <erik.hornvall@taftea.se>
2017-04-22 11:31:29 +0200

    vector class

--- Planet Game 4/Planet Game 4/Form1.cs
+++ Planet Game 4/Planet Game 4/Form1.cs
@@ -12,6 +12,9 @@ namespace Planet_Game_4
 {
     public partial class Form1 : Form
     {
+        BufferedGraphics BG;
+        PictureBox PB;
+        Timer T = new Timer();
         public Form1()
         {
             InitializeComponent();
@@ -19,14 +22,23 @@ namespace Planet_Game_4
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            display_text();
+            //initiate graphics
+            PB = new PictureBox();
+            PB.Parent = this;
+            PB.Location = new Point(0, 0);
+            WindowState = FormWindowState.Maximized;
+            PB.Dock = DockStyle.Fill;
+            BufferedGraphicsContext Context = BufferedGraphicsManager.Current;
+
+            //start the timer
+            T.Interval = 20;
+            T.Tick += T_Tick;
+            T.Start();
         }
 
-        public void display_text()
+        private void T_Tick(object sender, EventArgs e)//main loop
         {
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Eriksonn");
+            
         }
     }
 }
---
+++ Planet Game 4/Planet Game 4/Vector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planet_Game_4
+{
+    class Vector
+    {
+        double X;
+        double Y;
+        Vector()
+        {
+
+        }
+        Vector(double X,double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        Vector(Vector V)
+        {
+            X = V.X;
+            Y = V.Y;
+        }
+        static public Vector operator +(Vector V1,Vector V2)
+        {
+            return new Vector(V1.X + V2.X, V1.Y+V2.Y);
+        }
+        static public Vector operator -(Vector V1, Vector V2)
+        {
+            return new Vector(V1.X - V2.X, V1.Y - V2.Y);
+        }
+        static public Vector operator *(Vector V, double M)
+        {
+            return new Vector(V.X*M,V.Y*M);
+        }
+        static public Vector operator /(Vector V, double M)
+        {
+            return new Vector(V.X / M, V.Y / M);
+        }
+    }
+}
