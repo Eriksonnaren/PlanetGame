b1c7cdacbb3c500e735cd7529e47cb33b4193c55
John Hörnvall <john.minecraft@hotmail.se>
2017-04-22 11:51:17 +0200

    Added planets and space_bodies

---
+++ Planet Game 4/Planet Game 4/space_bodies/Planet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planet_Game_4
+{
+    public class Planet : space_bodies
+    {
+
+        public Orbit orbit;
+        public Vector position { get; set; }
+        public Vector velocity;
+
+        public double radius { get; set; }
+        public double mass { get; set; }
+
+        public Planet(Vector pos)
+        {
+            position = pos;
+        }
+
+        public void update(double dt)
+        {
+            
+        }
+
+        public void show()
+        {
+
+        }
+
+    }
+}
---
+++ Planet Game 4/Planet Game 4/space_bodies/space_bodies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planet_Game_4
+{
+    public interface space_bodies
+    {
+        
+
+        Vector position { get; set; }
+        
+        double radius { get; set; }
+        double mass { get; set; }
+
+    }
+}
