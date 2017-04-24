89d3c5f7d423fa999c5cc43c8507c62e92d9ca68
TrolledWoods <trolledwoods@gmail.com>
2017-04-24 09:03:44 +0200

    Commented most of the vector functions up.

--- Planet Game 4/Planet Game 4/Vector.cs
+++ Planet Game 4/Planet Game 4/Vector.cs
@@ -9,67 +9,122 @@ namespace Planet_Game_4
 {
     public class Vector
     {
+        ///<summary>
+        /// The x position stored in the vector
+        ///</summary>
         public double X;
+        ///<summary>
+        /// The y position stored in the vector
+        ///</summary>
         public double Y;
 
+        ///<summary>
+        /// This constructor literally just makes the object. Nothing to pass in.
+        ///</summary>
         public Vector()
         {
          
         }
 
+        ///<summary>
+        /// This constructor just copies the paramaters to the values inside the vector
+        ///</summary>
         public Vector(double X,double Y)
         {
             this.X = X;
             this.Y = Y;
         }
+        ///<summary>
+        /// This constructor copies another vector
+        ///</summary>
         public Vector(Vector V)
         {
             X = V.X;
             Y = V.Y;
         }
+        ///<summary>
+        /// This constructor copies the values from a point. TODO: do the same thing with pointF
+        ///</summary>
         public Vector(Point V)
         {
             X = V.X;
             Y = V.Y;
         }
+        
+        // This is what happens when you add two vectors together.
         static public Vector operator +(Vector V1,Vector V2)
         {
+            // It just returns the sum of the two vectors. Straight-forward!
             return new Vector(V1.X + V2.X, V1.Y+V2.Y);
         }
+        
+        // This is what happens when you subtract two vector
         static public Vector operator -(Vector V1, Vector V2)
         {
+            // It just returns the difference of the two vectors. Again, straight-forward
             return new Vector(V1.X - V2.X, V1.Y - V2.Y);
         }
+        
+        // This is what happens when you invert the sign of a vector
         static public Vector operator -(Vector V1)
         {
+            // It literally just inverts the x and y property. This is too straight-forward!
             return new Vector(-V1.X, -V1.Y);
         }
+        
+        // This scales a vector by a M
         static public Vector operator *(Vector V, double M)
         {
+            // The x and y values get multiplied by M
             return new Vector(V.X*M,V.Y*M);
         }
+        
+        // This scales a vector by 1/M
         static public Vector operator /(Vector V, double M)
         {
+            // Just divides the x and y values of the vector by M
             return new Vector(V.X / M, V.Y / M);
         }
+        
+        // Returns the magnitude^2. This is more efficient than just the magnitude, since it doesn't need a sqrt()
         public double MagSq()
         {
+            // Using phytagoras without the sqrt to make it squared
             return X * X + Y * Y;
         }
+        
+        /// <summary>
+        /// Returns the magnitude of the vector.
+        /// </summary>
         public double Mag()
         {
-            return Math.Sqrt(MagSq());
+            // Simply phytagoras
+            return Math.Sqrt(X * X + Y * Y); // Note to Erik: I think it's more messy to make a function call instead of just doing the humble math once again. It's not much math, anyway.
         }
+        
+        /// <summary>
+        /// Makes the vectors magnitude one.
+        /// </summary>
         public Vector Norm()
         {
+            // If the x and y values are 0, return a simple vector. This is to avoid divide by zero error
             if (X == 0 && Y == 0)
                 return new Vector(1, 0);
+            
+            // Return the normalized version of this vector.
             return this / Mag();
         }
+        
+        /// <summary>
+        /// Sets the magnitude of the vector to M
+        /// </summary>
         public Vector setMag(double M)
         {
+            // Simply normalize to length 1 and then scale by M. Simple and elegant :D
             return Norm() * M;
         }
+        
+        
         public double Dot(Vector V)
         {
             return X * V.X + Y * V.Y;
